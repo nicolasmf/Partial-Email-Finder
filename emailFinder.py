d47388501d65444597e0961bac1e4952e2df9c55
Nicolas MF <52258510+nicolasmf@users.noreply.github.com>
2020-08-24 23:43:07 +0200

    Fixed Instagram

--- emailFinder.py
+++ emailFinder.py
@@ -13,7 +13,7 @@ def twitter(email):
 def instagram(email):
     with open(combolist) as myFile:
         for num, line in enumerate(myFile, 1):
-            if email[0] == line.split(':')[0][0] and email[1] == line.split(':')[0][1] and len(email) == len(line.split(':')[0]) and email[email.find('@'):len(email)] == line.split(':')[0][line.find('@'):len(line.split(':')[0])]:
+            if email[0] == line.split(':')[0][0] and email[email.find('@')-1] == line.split(':')[0][line.find('@')-1] and len(email) == len(line.split(':')[0]) and email[email.find('@'):len(email)] == line.split(':')[0][line.find('@'):len(line.split(':')[0])]:
                 print("[*] Found at line",num,':')
                 password = line.split(':')
                 print("Email:", password[0])
